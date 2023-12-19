23e63ffa8dcac5685448b29b4c75b66343890616
Jan Woźniak <jnwznk@gmail.com>
2023-12-19 23:00:22 +0100

    Skip first argument as it is path to the executable

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,19 +1,20 @@
-use std::fs::read_to_string;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn merge_first_and_last_digit(line: &str) -> Option<u32> {
     let first = line.chars().find(|c| c.is_ascii_digit())?;
     let last = line.chars().rev().find(|c| c.is_ascii_digit())?;
-    // let num = format!("{}{}", first, last).parse::<u32>().ok();
-    // println!("{:?}", num);
-    // num
-    None
+    format!("{}{}", first, last).parse::<u32>().ok()
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let filename = std::env::args().next().unwrap();
-    let sum = read_to_string(filename)?
+    let filepath = std::env::args().nth(1).unwrap();
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    let sum = reader
         .lines()
-        .flat_map(merge_first_and_last_digit)
+        .flat_map(|line| merge_first_and_last_digit(&line.unwrap()))
         .sum::<u32>();
     println!("{}", sum);
 
