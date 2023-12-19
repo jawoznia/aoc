3d9b6ffa4c623d833746cb030610d3d58cf6bdff
Jan Woźniak <jnwznk@gmail.com>
2023-12-19 23:06:27 +0100

    refactor

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,22 +1,25 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn merge_first_and_last_digit(line: &str) -> Option<u32> {
+fn find_number(line: &str) -> Option<u32> {
     let first = line.chars().find(|c| c.is_ascii_digit())?;
     let last = line.chars().rev().find(|c| c.is_ascii_digit())?;
     format!("{}{}", first, last).parse::<u32>().ok()
 }
 
-fn main() -> Result<(), std::io::Error> {
-    let filepath = std::env::args().nth(1).unwrap();
+fn first_solution(filepath: String) -> Result<u32, std::io::Error> {
     let file = File::open(filepath)?;
     let reader = BufReader::new(file);
-
-    let sum = reader
+    Ok(reader
         .lines()
-        .flat_map(|line| merge_first_and_last_digit(&line.unwrap()))
-        .sum::<u32>();
-    println!("{}", sum);
+        .flat_map(|line| find_number(&line.unwrap()))
+        .sum::<u32>())
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let filepath = std::env::args().nth(1).unwrap();
+    let sum = first_solution(filepath)?;
+    println!("Sum: {}", sum);
 
     Ok(())
 }
